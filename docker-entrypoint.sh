7284501762cb02d40f4b4d92e0483ba112a768bf
stachdude <github@stach.co>
2017-03-29 11:31:50 +0200

    Remove: -e from command line

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -xe
 # If user don't provide any command
 # Run filebeat
 if [[ "$1" == "" ]]; then
-     exec filebeat -e -c /filebeat.yml -d "publish"
+     exec filebeat -c /filebeat.yml -d "publish"
 else
     # Else allow the user to run arbitrarily commands like bash
     exec "$@"
